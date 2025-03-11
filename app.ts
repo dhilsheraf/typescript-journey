0aee02552b1d7e75edc60ca4c1b2419d596d79e8
dhilsheraf <dhilsherafummer@gmail.com>
2025-03-12 01:29:55 +0530

    check rep

--- app.ts
+++ app.ts
@@ -21,3 +21,55 @@ function multiply(a:number,b:number,c?: number) :number{
     }
     return a*b;
 }
+
+let value : string | number
+
+// type Dog = { bark : ()=> void };
+
+//type 
+type userType = {
+    name:string
+    age:number
+}
+
+interface userIType  {
+    name:string
+    age:number
+}
+
+// animal interface interface inheritance called extends the interface
+
+interface Animal {
+    name:string
+}
+
+interface Dog extends Animal {
+
+    bark(): void;
+}
+
+const myDog: Dog = {
+    name:"Tomy",
+    bark: ()=>{
+       return console.log("bow bow bowww")
+    }
+}
+
+// doing it on the type
+type Bird = {
+    name:string
+}
+
+type Hen =  Bird & {
+    bark: ()=> void
+}
+
+const myHen : Hen ={
+    name:"Naadan",
+    bark:()=> console.log('kokara ko ko')
+}
+
+// immutable array
+const numbers : readonly number[] = [1,2,3,4,5,6]
+
+//
